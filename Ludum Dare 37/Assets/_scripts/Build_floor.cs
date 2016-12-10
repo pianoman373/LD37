0b3ee4c7a29eab6492693f62f0316bb9f2d70ed1
ParkerMc <nick@reisingerfamily.net>
2016-12-10 14:51:14 -0600

    Added floor and a lot more files by the look of it

---
+++ Ludum Dare 37/Assets/_scripts/Build_floor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Build_floor : MonoBehaviour {
+	// Use this for initialization
+	public GameObject prefab;
+	public int size;
+	public static GameObject[,] floor = new GameObject[20,20];
+
+	void Start () {
+		for (int x=0;x<size; x++) {
+			for (int y = 0; y < size; y++) {
+				floor[x,y] = Instantiate (prefab, new Vector3 (x, 0, y), new Quaternion ()) as GameObject;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
