01c59a3700240ab4480d0191a06ad5d34e340660
ParkerMc <nick@reisingerfamily.net>
2016-12-10 16:14:53 -0600

    0,0 should move up

--- Assets/_scripts/Build_floor.cs
+++ Assets/_scripts/Build_floor.cs
@@ -11,22 +11,35 @@ public class Build_floor : MonoBehaviour {
 
 	public static GameObject[,] floor = new GameObject[20,20];
 	public static bool[,] up = new bool[20, 20];
-	public static Vector3[] moveDown = new Vector3[400];
-	public static Vector3[] moveUp = new Vector3[400];
+	public static List<Vector2> moveDown = new List<Vector2>();
+	public static List<Vector2> moveUp = new List<Vector2>();
 
 	void Start () {
 		for (int x=0;x<size; x++) {//loop though x and y
 			for (int y = 0; y < size; y++) {
 				up[x,y] = false; //set all to false
-				floor[x,y] = Instantiate (prefab, new Vector3 (x, 0, y), new Quaternion ()) as GameObject;//and add all
+				floor[x,y] = Instantiate (prefab, new Vector3 (x, 0, y), new Quaternion ());//and add all
 			}
 		}
+		moveUp.Add(new Vector3(0,0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		foreach(Vector3 i in moveUp){
-			
+		float y;
+		foreach (Vector3 i in moveUp) {
+			if (floor [(int)i.x, (int)i.z].transform.position.y + (speed * Time.deltaTime) >= 1) {
+				y = 1;
+			} else {
+				y = (float)(floor [(int)i.x, (int)i.y].transform.position.y + (speed * Time.deltaTime));
+			}
+			floor [(int)i.x, (int)i.z].transform.position.Set (i.x, y, i.z);
+			print (y);
+			print (floor [(int)i.x, (int)i.y].transform.position);
+			if (i.y == 1) {
+				print ("remved");
+				moveUp.Remove (i);
+			}
+		}
 	}
-}
 }
