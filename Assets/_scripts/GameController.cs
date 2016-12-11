c14bc86edd1e98026b597b3c02ca06f32ab190c9
pianoman373 <jholley373@yahoo.com>
2016-12-11 03:38:56 -0600

    size is now const and array sizes are based off it.

--- Assets/_scripts/GameController.cs
+++ Assets/_scripts/GameController.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 	// Use this for initialization
 	public GameObject prefab;
-	public int size;//max is 20 for now
-	public float speed;
+    public float speed;
 
-	public static GameObject[,] floor = new GameObject[20,20];
-	public static bool[,] up = new bool[20, 20];
+    const int size = 10;//max is 20 for now
+	
+
+	public static GameObject[,] floor = new GameObject[size,size];
+	public static bool[,] up = new bool[size, size];
 	public static List<Vector2> moveDown = new List<Vector2>();
 	public static List<Vector2> moveUp = new List<Vector2>();
 
